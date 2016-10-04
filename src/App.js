bd6da6633d0b99416ca6921047a35568912c5ebd
Joost VanDorp <joostvandorp@gmail.com>
2016-10-04 17:45:54 -0400

    Be A test branch
    learning react

--- src/App.js
+++ src/App.js
@@ -3,19 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+        };
+    }
+
+    componentWillMount(){
+        var self = this;
+
+        fetch(
+            'https://loyalty-engine-qa.vandorp.io/GDILoyalty/v1.0/en/loyalty/details/?format=json'
+        ).then(
+            function(res) {
+                return res.json()
+            }
+        ).then(
+            function(json) {
+                self.setState({ data: json })
+                self.setState({ name: "Joost VanDorp"})
+            }
+        ).catch(
+            function(error) {
+                console.log(error)
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className="App">
+
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+
+                <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+
+                <p>
+                {this.state.name}
+                </p>
+
+            </div>
+        );
+    }
 }
 
 export default App;
+
+
+
+
