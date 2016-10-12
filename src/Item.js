b67709a424216e5320d5ee01a77cea51e5cf9b2e
malachiconstant <jon.manalo@galepartners.com>
2016-10-12 16:52:33 -0400

    overlay added

--- src/Item.js
+++ src/Item.js
@@ -8,12 +8,18 @@ class Item extends Component {
     this.state = {
       isLoading: true,
       data: [],
-    }
+      overlay: false,
+      didInvalidate: true,
+      overlayHeight: 0,
+      bidInput: 0
+    };
+    this._handleChange= this._handleChange.bind(this);
+    this._overlay = this._overlay.bind(this);
+    this._cancelBid = this._cancelBid.bind(this);
   }
-
   componentWillMount() {
     return {
-      data: ""
+      data: "",
     }
   }
   componentDidMount() {
@@ -26,10 +32,40 @@ class Item extends Component {
           isLoading: false,
         })
       })
+
+      window.addEventListener('resize', () => this._onResize());
   }
   createMarkup(html) {
     return {__html: html};
   }
+  _handleChange(event) {
+    this.setState({
+      bidInput: event.target.value
+    });
+  }
+  _overlay() {
+    this.setState({
+      overlay: true
+    })
+  }
+  _cancelBid() {
+    this.setState({
+      overlay: false
+    })
+  }
+  _onResize() {
+      this.setState({
+        didInvalidate: true });
+  }
+  _overlayCover(el){
+    if(el && this.state.didInvalidate) {
+      this.setState({
+        didInvalidate: false,
+        overlayHeight: el.clientHeight
+      })
+    }
+  }
+
   render() {     
     if (this.state.isLoading) {
       return (
@@ -40,9 +76,18 @@ class Item extends Component {
     var backgroundImage = {
       backgroundImage: 'url(' + this.state.data[0].image + ')'
     }
-    console.log(this.state.data[0].image);
+    var overlayStyle = {
+      height: this.state.overlayHeight
+    }
+    var overlayTop = {
+      top: (document.body.scrollTop) + (window.innerHeight * .1)
+    }
+    var valueLink = {
+      value: this.state.bidInput,
+      requestChange: this._handleChange
+    }
     return (
-      <div className="item-wrapper">
+      <div className="item-wrapper" ref={this._overlayCover.bind(this)}>
         <Header />
         <div className="hero-image-wrapper" style={backgroundImage}>
             <div className="text-container">
@@ -64,9 +109,9 @@ class Item extends Component {
                 <span>Price value (min ${this.state.data[0].retail_value})</span>
               </div>
               <span className="bid-input">
-                <input type="number" min={this.state.data[0].minimum_increase} />
+                <input type="number" min={this.state.data[0].minimum_increase} placeholder={this.state.data[0].minimum_increase} onChange={this._handleChange}/>
               </span>
-              <div className="submit-button">
+              <div className="submit-button" onClick={this._overlay}>
                 <span>
                   Bid Now
                 </span>
@@ -74,6 +119,20 @@ class Item extends Component {
             </div> 
           </div>
 
+        </div>
+        <div className={"overlay-container " + (this.state.overlay ? 'active' : '')} style={overlayStyle}>
+            <div className="overlay-inside" style={overlayTop}>
+            <div className="cancel-container" onClick={this._cancelBid}>
+              <div className="cancel">
+              </div>
+            </div>
+              <span>Are you sure you want to bid <span>${this.state.bidInput === '' || this.state.bidInput < this.state.data[0].minimum_increase ? this.state.data[0].minimum_increase : this.state.bidInput }</span>?</span>
+              <div className="submit-button">
+                <span>
+                  Bid Now
+                </span>
+              </div>
+            </div>
         </div>
       </div>
     );
